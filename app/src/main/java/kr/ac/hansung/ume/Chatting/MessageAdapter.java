f6c3fc726f4406dd42e942b2fcfd7c6601745ec7
nahyunLee <sksms304@gmail.com>
2019-11-29 13:33:29 +0900

    test2

---
+++ app/src/main/java/kr/ac/hansung/ume/Chatting/MessageAdapter.java
@@ -0,0 +1,27 @@
+package kr.ac.hansung.ume.Chatting;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MessageAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
